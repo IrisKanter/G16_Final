8826e89da48a5326fcb26f4f626f6af7f3c931d8
OfirBraude <ofir.uzie.@e.braude.ac.il>
2024-08-16 17:56:12 +0300

    server fix

--- G16_Server/server.js
+++ G16_Server/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use('/api/quizzes', quizRoutes);
 
 // MongoDB Connection
-const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/quizDB';
+const mongoURI = process.env.MONGO_URI;
 mongoose.connect(mongoURI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Failed to connect to MongoDB', err));
